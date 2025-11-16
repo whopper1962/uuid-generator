6dc16e6733635a3e2d6ea1a2898f177ac8d8df33
Masashi Kawakami <whopper1962js@gmail.com>
2025-11-16 11:42:49 +0900

    chore(storybook): set up Storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import "../src/app/globals.css";
+import type { Preview } from "@storybook/nextjs";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
