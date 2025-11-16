30e802a1bbb54fdbbadd41ad6fad91704f543ca8
Masashi Kawakami <whopper1962js@gmail.com>
2025-11-16 11:41:14 +0900

    fix(ui-tooltip): adjust font-weight in Tooltip component

--- src/components/ui/tooltip/tooltip.tsx
+++ src/components/ui/tooltip/tooltip.tsx
@@ -3,12 +3,21 @@
 import {
   Tooltip as FlowbiteTooltip,
   TooltipProps as FlowbiteTooltipProps,
+  createTheme,
 } from "flowbite-react";
 
 export interface TooltipProps extends FlowbiteTooltipProps {
   children: React.ReactNode;
 }
 
+const customTheme = createTheme({
+  base: "font-normal",
+});
+
 export const Tooltip = ({ children, ...props }: TooltipProps) => {
-  return <FlowbiteTooltip {...props}>{children}</FlowbiteTooltip>;
+  return (
+    <FlowbiteTooltip {...props} theme={customTheme}>
+      {children}
+    </FlowbiteTooltip>
+  );
 };
